688a6c17b6b8f600b1547681222da9afe4a45741
Krupaee-S <krupaeesharmawork@gmail.com>
2026-01-19 23:45:38 +0530

    while loop

---
+++ While_loop.py
@@ -0,0 +1,44 @@
+# count = 17
+# while count <= 170:
+#     print(count)
+#     count += 17
+
+# answer = 17
+# number = 1
+# while answer <= 170 and number <= 10:
+#     print (f'17 X {number} = {answer}')
+#     answer += 17
+#     number += 1
+
+# answer = ""
+# while answer != "yes":
+#     answer = input("Do you agree? (yes/no): ")
+# print ('Thank you')
+
+# answer = ""
+# while answer != "yes":
+#     answer = input("Have you had dinner? (yes/no): ")
+# print ('Good')
+
+# while True:
+#     print(" I'm unstoppable! ")
+
+
+# while True:
+#     answer = input("Do you agree? (yes/no): ")
+#     if  answer == "yes":
+#         break
+# print ('Thank you')
+
+# Challenge
+attempts = 0
+while attempts < 3 :
+     answer = input("Do you agree? (yes/no): ")
+     if answer == "yes":
+          print ("Glad we are on the same page")
+          break
+     attempts += 1
+else:
+     print("3 strikes, You are out!")    
+
+
