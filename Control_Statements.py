c9a428148ccc10368a30b5099af5f3a34d7b2564
Krupaee-S <krupaeesharmawork@gmail.com>
2026-01-19 20:48:05 +0530

    updated code

--- Control_Statements.py
+++ Control_Statements.py
@@ -77,34 +77,52 @@
 
 
 # CHALLENGE 
-email = "Kru@paee@gmail.com!"
-valid = True
-# if-else is not used here because then the o/p would be just one error at a time |rather| Independent if statements are used so that all the errors are showed at once
-# clean the string
-email = email.strip()
-# Email must not contain spaces
-if email == "":
-    print("Email cannot be empty")
-    valid = False
-# Email must contain a '.' and '@'
-if not('.' in email and '@' in email):
-    print("Email must contain a '.' and '@'")
-    valid = False
-# Email must contain only one'@'
-if email.count('@') != 1 : # != -> not equals to
-    print(" Email must contain exactly one @.")
-    valid = False
-# Email must end with '.com', '.org', or '.net'
-if not email.endswith(('.com', '.org', '.net')):
-    print("Email must end with '.com', '.org', or '.net'")
-    valid = False
-# Email must not be longer than 254 characters
-if len(email)> 254:
-    print("Email must not be longer than 254 characters")
-    valid = False
-# Email must start and end with a letter or a digit
-if not(email[0].isalnum() and email[-1].isalnum()):
-    print("Email must start or end with a letter or a digit")
-    valid = False
-if valid:
-    print("Email is valid")
+# email = "Kru@paee@gmail.com!"
+# valid = True
+# # if-else is not used here because then the o/p would be just one error at a time |rather| Independent if statements are used so that all the errors are showed at once
+# # clean the string
+# email = email.strip()
+# # Email must not contain spaces
+# if email == "":
+#     print("Email cannot be empty")
+#     valid = False
+# # Email must contain a '.' and '@'
+# if not('.' in email and '@' in email):
+#     print("Email must contain a '.' and '@'")
+#     valid = False
+# # Email must contain only one'@'
+# if email.count('@') != 1 : # != -> not equals to
+#     print(" Email must contain exactly one @.")
+#     valid = False
+# # Email must end with '.com', '.org', or '.net'
+# if not email.endswith(('.com', '.org', '.net')):
+#     print("Email must end with '.com', '.org', or '.net'")
+#     valid = False
+# # Email must not be longer than 254 characters
+# if len(email)> 254:
+#     print("Email must not be longer than 254 characters")
+#     valid = False
+# # Email must start and end with a letter or a digit
+# if not(email[0].isalnum() and email[-1].isalnum()) :
+#     print("Email must start or end with a letter or a digit")
+#     valid = False
+# if valid:
+#     print("Email is valid")
+
+
+def main(): 
+    name = "Krupaee"
+# name.count("e"):
+#         print(name.count("e"))
+    if 'e' in name:
+        print(name.count("e"))
+    elif len(name) == 5:
+        print(" longer")
+   
+ 
+
+
+
+if __name__ == "__main__":
+    main()
+
--- For_Loop.py
+++ For_Loop.py
@@ -29,6 +29,33 @@
 #     for i in [1,2,3,4,5,6,7,8,9,10]:
 #         print(f"7 X {i} = {table}")
 
-item = "*"
-for item in range(1,7,1):
-    print (f"{item}")
+# item = "*"              PROBLEM
+# for item in range(1,7,1):
+#     print (f"{item}")
+
+# Nested for-loop
+# for x in range (3):
+#     for y in range (2):
+#         for z in range (2):
+#             print (f"{x}, {y}, {z}")
+
+# colors =['red', 'green', 'blue']
+# sizes = ['L', 'M', 'S']
+# for color in colors:
+#     for size in sizes:
+#         print(f"{color} - size {size}")
+
+# years =[2026, 2027]
+# months =['Jan', 'Feb']
+# days = range(1,29)
+# for y in years:
+#     for m in months:
+#         for d in days:
+#             print(f"report_{y}_{m}_{d}.csv")
+
+# it is an SQL query which is made easy with use of python
+# tables = ['customers', ' orders', 'products', 'prices']
+# columns = ['ID', 'create_date']
+# for t in tables:
+#     for c in columns:
+#         print(f'SELECT count(*) FROM {t} WHERE {c} IS NULL;')
