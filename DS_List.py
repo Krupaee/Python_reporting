142dd68ff381bcaf102563cdb38b122f61b4661d
Krupaee-S <krupaeesharmawork@gmail.com>
2026-01-20 00:32:13 +0530

    new learnings

---
+++ DS_List.py
@@ -0,0 +1,62 @@
+# empty = []
+# print(empty)
+# print(type(empty))
+
+# empty = []
+# letters = ['a', 'b', 'c']
+# print(letters)
+# print (type(letters))
+
+# empty = list()
+# print(empty)
+
+# letters = 'python'
+# print(letters)
+
+# letters = list('python')
+# print (letters)
+
+# numbers = list(range(7))
+# print(numbers)
+
+# nested list
+# matrix = [
+#     ['a', 'b', 'c'],
+#     ['d', 'e', 'f']
+# ]
+# print (matrix)
+# print(type(matrix))
+
+# Indexing
+# lst = ['a', 'b', 'c', 'd']
+# print (lst)
+# print (lst[0])
+# print (lst[-1])
+
+# matrix = [
+#     ['a', 'b', 'c'],
+#     ['d', 'e', 'f'],
+#     ['g', 'h', 'i']
+# ]
+# # print (matrix)
+# print (matrix[-1])
+# print (matrix[-1][-2])
+# print (matrix[-1][2])
+# print (matrix[-1][-1])
+# print (matrix[0][0])
+# print (matrix[1][1])
+
+# Slicing
+# lst = ['a', 'b', 'c', 'd']
+# print (lst[:2])
+# print (lst[2:])
+
+# matrix = [
+#      ['a', 'b', 'c'],
+#      ['d', 'e', 'f'],
+#      ['g', 'h', 'i']
+# ]
+# print(matrix[:2])
+# print(matrix[2:])
+
+# print(matrix[2][:2])
